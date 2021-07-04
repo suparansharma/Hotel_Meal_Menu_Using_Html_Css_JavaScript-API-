99c41c3da85426435f0d8b830949db55bd4813fe
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-04 23:54:49 +0600

    using map

--- app.js
+++ app.js
@@ -12,34 +12,51 @@
 // }
 
 
+//search Button
 
-document.getElementById('submit-btn').addEventListener('click' , function () {
-    var mealName = document.getElementById('meal-name-input').value;
-    console.log(mealName);
-
-
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
-    console.log(url);
-
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            var MealDetails = data.meals[0];
-            document.getElementById('meal-name').innerText = MealDetails.strMeal;
-            document.getElementById('meal-img').src = MealDetails.strMealThumb;
-
-            // let weatherTempInKelvin = data.main.temp;
-            // let weatherTempInCelsius = Math.round(weatherTempInKelvin - 273.15);
+// document.getElementById('submit-btn').addEventListener('click' , function () {
+//     var mealName = document.getElementById('meal-name-input').value;
+//     console.log(mealName);
 
 
-            // document.getElementById('city-temp').innerText = weatherTempInCelsius;
+//     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
+//     console.log(url);
 
-            // document.getElementById('city-weather').innerText = weatherDetails.main;
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => {
+//             var MealDetails = data.meals[0];
+//             document.getElementById('meal-name').innerText = MealDetails.strMeal;
+//             document.getElementById('meal-img').src = MealDetails.strMealThumb;            
+//             console.log(strMealThumb);})
+// })
 
-            // let iconSet = MealDetails.strMealThumb;
-            // let strMealThumb = `https://www.themealdb.com/images/media/meals/${iconSet}.jpg`;
-            // document.getElementById('meal-img').src = strMealThumb;
-            
-            
-            console.log(strMealThumb);})
-})
+
+
+
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+.then(res => res.json())
+.then(data => displayMeals(data))
+
+
+function displayMeals(categories){
+
+const meal = categories.categories;
+  
+meal.map(categorie =>console.log(categorie));
+//console.log(meal);
+
+}
+
+
+// const displayMeals = meals =>{
+
+//     console.log(meals);
+    
+
+//     for (let i = 0; i < meals.length; i++) {
+//         const meal = meals[i];
+//         console.log(meal.strCategory);
+        
+//     }
+// }
