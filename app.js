7e6384be1ab121fa980e1caa5c367a3bbcc30029
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-05 04:34:04 +0600

    catagories all value

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-// fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-// .then(res => res.json())
-// .then(data => displayMeals(data));
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+.then(res => res.json())
+.then(data => displayMeals(data));
 
-// const displayMeals = meals =>{
+const displayMeals = categories =>{
 
-//     for (let i = 0; i < meals.length; i++) {
-//         const meal = meals[i];
-//         console.log(meal.categories);
+
+
+    const categoriesItem = categories.categories;
+    console.log(categoriesItem);
+    const categoriesDiv = document.getElementById('categories-chart');
+    for (let i = 0; i < categoriesItem.length; i++) {
+        const categoriesValue =categoriesItem[i];
+
+        const categoryDiv  = document.createElement('div');
+
+        //console.log(categoriesValue.strCategory);
+
         
-//     }
-// }
+
+        // const h3 = document.createElement('h3');
+        // h3.innerText =  categoriesValue.strCategory;
+        // categoriesDiv.appendChild(h3);
+
+
+        // const p = document.createElement('p');
+        // p.innerText =  categoriesValue.strCategoryDescription;
+        // categoriesDiv.appendChild(p);
+
+
+        // const images = document.getElementById('categories-image');
+        // images.src =  categoriesValue.strCategoryThumb;
+        
+
+
+
+
+
+
+        const categoriesInfo = `
+            <h1>${categoriesValue.strCategory}</h1>
+             <p>${categoriesValue.strCategoryDescription}</p>
+             <img src="${categoriesValue.strCategoryThumb}"></img>
+           
+           `
+           categoryDiv.innerHTML = categoriesInfo;
+           categoriesDiv.appendChild(categoryDiv);
+
+        //    <img src="${categoriesValue.strCategoryThumb}"></img>
+
+    }
+}
 
 
 //search Button
@@ -34,36 +74,55 @@
 
 
 
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => displayCategoriesMeals(data))
+// fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+//     .then(res => res.json())
+//     .then(data => displayCategoriesMeals(data))
 
 
-function displayCategoriesMeals(categories){
+// function displayCategoriesMeals(categories) {
 
-const mealItem = categories.categories;
-const div = document.getElementById('categories-chart');
-  
-mealItem.map(categorie =>{
+//     const mealItem = categories.categories;
+//     const div = document.getElementById('categories-chart');
 
-    const h3 = document.createElement('h3');
-    h3.innerText =  categorie.strCategory;
-    div.appendChild(h3);
-    console.log(categorie.strCategory);
-});
-//console.log(meal);
+//     mealItem.map(categorie => {
 
-}
+//         const image = document.createElement('image');
+//         image.innerHTML =  categorie.strCategoryThumb;
+//         div.appendChild(image);
+
+//         const h3 = document.createElement('h3');
+//         h3.innerText =  categorie.strCategory;
+//         div.appendChild(h3);
+
+//         const p = document.createElement('p');
+//         p.innerText =  categorie.strCategoryDescription;
+//         div.appendChild(p);
+
+
+
+
+// //         const categoriesInfo = `
+// //     <h1>${categorie.strCategory}</h1>
+// //     <p>${categorie.strCategoryDescription}</p>
+// //     <img src="${categorie.strCategoryThumb}">
+// //   `
+// //         div.innerHTML = categoriesInfo;
+
+// //         console.log(categorie.strCategory);
+//     });
+//     //console.log(meal);
+
+// }
 
 
 // const displayMeals = meals =>{
 
 //     console.log(meals);
-    
+
 
 //     for (let i = 0; i < meals.length; i++) {
 //         const meal = meals[i];
 //         console.log(meal.strCategory);
-        
+
 //     }
 // }
