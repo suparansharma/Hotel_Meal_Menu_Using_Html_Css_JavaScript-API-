2f67968d3a4c9e82b29d7eabb6b6106f06bb2f0c
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-07-04 21:02:17 +0600

    search work done

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-.then(res => res.json())
-.then(data =>{
-    const mealName = data.meals[0]
-    console.log(mealName.strMeal)
-
-})
+// fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+// .then(res => res.json())
+// .then(data => displayMeals(data));
 
 // const displayMeals = meals =>{
 
 //     for (let i = 0; i < meals.length; i++) {
 //         const meal = meals[i];
-//         console.log(meal);
+//         console.log(meal.categories);
         
 //     }
-    
-
 // }
+
+
+
+document.getElementById('submit-btn').addEventListener('click' , function () {
+    var mealName = document.getElementById('meal-name-input').value;
+    console.log(mealName);
+
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
+    console.log(url);
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            var MealDetails = data.meals[0];
+            document.getElementById('meal-name').innerText = MealDetails.strMeal;
+            document.getElementById('meal-img').src = MealDetails.strMealThumb;
+
+            // let weatherTempInKelvin = data.main.temp;
+            // let weatherTempInCelsius = Math.round(weatherTempInKelvin - 273.15);
+
+
+            // document.getElementById('city-temp').innerText = weatherTempInCelsius;
+
+            // document.getElementById('city-weather').innerText = weatherDetails.main;
+
+            // let iconSet = MealDetails.strMealThumb;
+            // let strMealThumb = `https://www.themealdb.com/images/media/meals/${iconSet}.jpg`;
+            // document.getElementById('meal-img').src = strMealThumb;
+            
+            
+            console.log(strMealThumb);})
+})
